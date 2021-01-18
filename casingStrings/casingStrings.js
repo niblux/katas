87ce0c0da1c3b4d4d5cc3e0bf9fddcf0b4b6d7e7
Nabil Elidrissi <niblux2004@yahoo.co.uk>
2021-01-18 22:33:11 +0000

    initial commit, with first kata added

---
+++ casingStrings/casingStrings.js
@@ -0,0 +1,10 @@
+String.prototype.toJadenCase = function (quote) {
+    //...
+    // convert into an array, by splicing by the space in the quote.
+    
+    // loop over the new array, use string replace to capitlize first letter . 
+    
+    // take a word and captilize it 
+  };
+
+  // last left off, using a regex to pick up the first letter of each word in the array. 
